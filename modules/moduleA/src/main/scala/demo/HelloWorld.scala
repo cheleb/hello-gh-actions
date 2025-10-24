4d9dabd70d301264d2d4d3f1f090c8e066cdecb5
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-10-24 20:58:55 +0200

    Update print statements in HelloWorld and Test for consistency

--- modules/moduleA/src/main/scala/demo/HelloWorld.scala
+++ modules/moduleA/src/main/scala/demo/HelloWorld.scala
@@ -1,5 +1,5 @@
 package demo
 
 object HelloWorld extends App {
-  println("Hello, world 4!")
+  println("Hello, world 5!")
 }
--- modules/moduleA/src/test/scala/demo/Test.scala
+++ modules/moduleA/src/test/scala/demo/Test.scala
@@ -1,5 +1,5 @@
 package demo
 
 object Test {
-  println("This is a test 4!")
+  println("This is a test 5!")
 }
